8cc62980dccd089d42a37167cf18ba12002b0738
ZaitonZz <philipcandelario1@gmail.com>
2023-12-08 16:36:07 +0800

    added most of readscreen

---
+++ app/src/main/java/com/example/sagesayings/ReadScreen.java
@@ -0,0 +1,14 @@
+package com.example.sagesayings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReadScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_screen);
+    }
+}
