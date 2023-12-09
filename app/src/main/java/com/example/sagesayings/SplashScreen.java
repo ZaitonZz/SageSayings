e056029336fc125799444b66007e5ffef07c4d0f
ZaitonZz <philipcandelario1@gmail.com>
2023-12-09 22:20:51 +0800

    finished readScreen and audioScreen, started vot

--- app/src/main/java/com/example/sagesayings/SplashScreen.java
+++ app/src/main/java/com/example/sagesayings/SplashScreen.java
@@ -1,14 +1,20 @@
 package com.example.sagesayings;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
         setContentView(R.layout.activity_splash_screen);
+
+
     }
 }
